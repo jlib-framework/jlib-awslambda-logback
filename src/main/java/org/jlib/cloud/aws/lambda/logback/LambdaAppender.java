ab2dfdcf811f167db27689471f81de3e54bb8b88
Igor Akkerman <igor.akkerman@jlib.org>
2018-07-15 22:34:38 +0200

    jlib-awslambda-logback implemented, unit tests added

---
+++ src/main/java/org/jlib/cloud/aws/lambda/logback/LambdaAppender.java
@@ -0,0 +1,26 @@
+package org.jlib.cloud.aws.lambda.logback;
+
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.UnsynchronizedAppenderBase;
+import ch.qos.logback.core.encoder.Encoder;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.lambda.runtime.LambdaRuntime;
+import com.amazonaws.services.lambda.runtime.LambdaRuntimeInternal;
+import lombok.Setter;
+
+public class LambdaAppender extends UnsynchronizedAppenderBase<ILoggingEvent> {
+
+    private final LambdaLogger logger = LambdaRuntime.getLogger();
+
+    @Setter
+    private Encoder<ILoggingEvent> encoder;
+
+    public LambdaAppender() {
+        LambdaRuntimeInternal.setUseLog4jAppender(true);
+    }
+
+    @Override
+    protected void append(ILoggingEvent event) {
+        logger.log(encoder.encode(event));
+    }
+}
---
+++ src/test/java/org/jlib/cloud/aws/lambda/logback/LogTest.java
@@ -0,0 +1,119 @@
+package org.jlib.cloud.aws.lambda.logback;
+
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.encoder.PatternLayoutEncoder;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static java.lang.System.lineSeparator;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Slf4j
+public class LogTest {
+
+    private ByteArrayOutputStream byteOut;
+    private PrintStream outMock;
+    private PrintStream out;
+
+    @BeforeEach
+    public void mockStandardOut() {
+        byteOut = new ByteArrayOutputStream();
+        outMock = new PrintStream(byteOut);
+
+        out = System.out;
+
+        System.setOut(outMock);
+    }
+
+    @AfterEach
+    public void revertStandardOut() {
+        System.setOut(out);
+    }
+
+    @Test
+    public void xmlConfigNoRequestId() {
+
+        // given
+        // logback.xml present
+
+        // when
+        log.info("14m6d4 15 höt");
+
+        // then
+        assertThat(byteOut.toString())
+                .matches("\\[\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2}\\.\\d{3}\\] NO-REQUEST-ID INFO o.j.c.a.l.l.LogTest - 14m6d4 15 höt" + lineSeparator() + "$");
+    }
+
+    @Test
+    public void xmlConfigDebugShouldNotBeLogged() {
+
+        // given
+        // logback.xml present
+
+        // when
+        log.debug("14m6d4 15 höt");
+
+        // then
+        assertThat(byteOut.toString())
+                .isEmpty();
+    }
+
+    @Test
+    public void xmlConfigMdcRequestId() {
+
+        String requestIdMdcKey = "AWSRequestId";
+
+        // given
+        // logback.xml present and
+        MDC.put(requestIdMdcKey, "jlib-15-c001");
+
+        // when
+        log.info("14m6d4 15 höt");
+
+        // then
+        assertThat(byteOut.toString())
+                .matches("\\[\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2}\\.\\d{3}\\] jlib-15-c001 INFO o.j.c.a.l.l.LogTest - 14m6d4 15 höt" + lineSeparator() + "$");
+
+        // cleanup
+        MDC.remove(requestIdMdcKey);
+    }
+
+    @Test
+    public void programmaticConfig() {
+
+        // given
+        LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
+        PatternLayoutEncoder encoder = new PatternLayoutEncoder();
+
+        encoder.setPattern("[%level] <%logger> %msg%n");
+        encoder.setContext(lc);
+        encoder.start();
+
+        LambdaAppender appender = new LambdaAppender();
+        appender.setEncoder(encoder);
+        appender.setContext(lc);
+        appender.start();
+
+        Logger logger = (Logger) LoggerFactory.getLogger("special.Logger");
+        logger.addAppender(appender);
+        logger.setLevel(Level.DEBUG);
+        logger.setAdditive(false); /* set to true if root should log too */
+
+        // when
+        logger.debug("14m6d4 15 höt");
+
+        // then
+        assertThat(byteOut.toString())
+                .isEqualTo("[DEBUG] <special.Logger> 14m6d4 15 höt" + lineSeparator());
+    }
+
+}
